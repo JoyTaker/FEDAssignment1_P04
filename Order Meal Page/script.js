785f078a9716bea46588d098e6a9d86d8ddb2b82
JoyTaker <kerwinliyy@gmail.com>
2024-11-29 21:05:01 +0800

    Use flex boxes and made the contact Us, inclusive of the icons of each social media platform

---
+++ Order Meal Page/script.js
@@ -0,0 +1,9 @@
+
+document.getElementById("homeID").addEventListener("click", function(){
+    window.location.href = "../HomePage/index.html";
+});
+
+
+document.getElementById("orderTrackingID").addEventListener("click", function(){
+    window.location.href = "../OrderTracking/index.html";
+});
---
+++ OrderTracking/script.js
@@ -0,0 +1,9 @@
+
+document.getElementById("homeID").addEventListener("click", function(){
+    window.location.href = "../HomePage/index.html";
+});
+
+document.getElementById("contactUsID").addEventListener("click", function(){
+    window.location.href = "../ContactUsPage/index.html";
+});
+
